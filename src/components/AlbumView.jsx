16b0b2302416dabfb215b14a3eb1009585c5102d
Marlon Baker <marbaker.swe@gmail.com>
2023-03-06 21:52:09 -0800

    results fixed

--- src/components/AlbumView.jsx
+++ src/components/AlbumView.jsx
@@ -1,6 +1,7 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about a given album
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 function AlbumView() {
     const [ albumData, setAlbumData ] = useState([])
